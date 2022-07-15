442140b6343df7eaf97f3c0f17d61dcca2eb6305
Vladislav Yakovlev <vlad309523@gmail.com>
2022-07-15 21:54:39 +0400

    Fix username parsing

--- src/utils/getUserName.ts
+++ src/utils/getUserName.ts
@@ -2,5 +2,5 @@ import { User } from 'grammy/out/platform.node';
 import * as R from 'remeda';
 
 export const getUserName = (user?: User) => {
-  return user ? user.username || [user.first_name, user.last_name].filter(R.isTruthy).join(' ') : '';
+  return user ? user.username?.replaceAll('_', '\\_') || [user.first_name, user.last_name].filter(R.isTruthy).join(' ') : '';
 };
